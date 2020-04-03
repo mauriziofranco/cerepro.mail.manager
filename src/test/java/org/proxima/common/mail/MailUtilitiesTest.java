7d78a10e88ed0d06471cf4ecc750f6e4df0cbbed
MaurizioFranco <maurizio.franco@ymail.com>
2020-04-03 11:35:49 +0200

    Code coverage testing!!

--- src/test/java/org/proxima/common/mail/MailUtilitiesTest.java
+++ src/test/java/org/proxima/common/mail/MailUtilitiesTest.java
@@ -31,95 +31,95 @@ public class MailUtilitiesTest {
 		}
 	}
 
-//	@Test
-//	public void sendSimpleMailOk() {
-//
-//		logger.info("#########");
-//		logger.info("TEST - sendSimpleMail()");
-//		logger.info("#########");
-//		Boolean check = false;
-//		check = MailUtility.sendSimpleMail(to, "Prova e-mail semplice",
-//				"<p class=\"abcde\">Test send simple mail avvenuto con successo! </p>");
-//		assertTrue(check);
-//	}
+	@Test
+	public void sendSimpleMailOk() {
+
+		logger.info("#########");
+		logger.info("TEST - sendSimpleMail()");
+		logger.info("#########");
+		Boolean check = false;
+		check = MailUtility.sendSimpleMail(to, "Prova e-mail semplice",
+				"<p class=\"abcde\">Test send simple mail avvenuto con successo! </p>");
+		assertTrue(check);
+	}
+
+	@Test
+	public void sendSimpleMailKo() {
+
+		logger.info("#########");
+		logger.info("TEST - sendSimpleMail()");
+		logger.info("#########");
+		Boolean check = false;
+		String[] recipients = null;
+		check = MailUtility.sendSimpleMail(recipients, "Prova e-mail semplice",
+				"<p class=\"abcde\">Test send simple mail avvenuto con successo! </p>");
+		assertFalse(check);
+	}
+
+	// test cc
+	@Test
+	public void sendSimpleMailWithCc() {
+
+		logger.info("#########");
+		logger.info("TEST - sendSimpleMailWithCc()");
+		logger.info("#########");
+		Boolean check = false;
+		check = MailUtility.sendSimpleMailWithCc(to, cc, "Prova e-mail con CC",
+				"<p class=\"abcde\">Test send simple mail con CC avvenuto con successo!</p>");
+		assertTrue(check);
+	}
 
 //	@Test
-//	public void sendSimpleMailKo() {
+//	public void sendSimpleMailWithCcSingleOk() {
 //
 //		logger.info("#########");
-//		logger.info("TEST - sendSimpleMail()");
+//		logger.info("TEST - sendSimpleMailWithCcMultiOk()");
 //		logger.info("#########");
 //		Boolean check = false;
-//		String[] recipients = null;
-//		check = MailUtility.sendSimpleMail(recipients, "Prova e-mail semplice",
-//				"<p class=\"abcde\">Test send simple mail avvenuto con successo! </p>");
-//		assertFalse(check);
-//	}
-//
-//	// test cc
-//	@Test
-//	public void sendSimpleMailWithCc() {
-//
-//		logger.info("#########");
-//		logger.info("TEST - sendSimpleMailWithCc()");
-//		logger.info("#########");
-//		Boolean check = false;
-//		check = MailUtility.sendSimpleMailWithCc(to, cc, "Prova e-mail con CC",
+//		check = MailUtility.sendSimpleMailWithCc(singleRecipient, singleRecipient, "Prova e-mail con CC",
 //				"<p class=\"abcde\">Test send simple mail con CC avvenuto con successo!</p>");
 //		assertTrue(check);
 //	}
-//
-////	@Test
-////	public void sendSimpleMailWithCcSingleOk() {
-////
-////		logger.info("#########");
-////		logger.info("TEST - sendSimpleMailWithCcMultiOk()");
-////		logger.info("#########");
-////		Boolean check = false;
-////		check = MailUtility.sendSimpleMailWithCc(singleRecipient, singleRecipient, "Prova e-mail con CC",
-////				"<p class=\"abcde\">Test send simple mail con CC avvenuto con successo!</p>");
-////		assertTrue(check);
-////	}
-//
-//	// test bcc
-//	@Test
-//	public void sendSimpleMailWithCcAndCcn() {
-//
-//		logger.info("#########");
-//		logger.info("TEST - sendSimpleMailWithCcAndCcn()");
-//		logger.info("#########");
-//		Boolean check = false;
-//		check = MailUtility.sendSimpleMailWithCcAndCcn(to, cc, ccn, "Prova e-mail con CC e CCN",
-//				"<p class=\"abcde\">Test send simple mail con CC e CCN avvenuto con successo!</p>");
-//		assertTrue(check);
-//	}
-//
-//	// test default cc
-//	@Test
-//	public void sendSimpleMailWithDefaultCc() {
-//
-//		logger.info("#########");
-//		logger.info("TEST - sendSimpleMailWithDefaultCc()");
-//		logger.info("#########");
-//
-//		Boolean check = false;
-//		check = MailUtility.sendSimpleMailWithDefaultCc(to, "Prova e-mail con default CC",
-//				"<p class=\"abcde\">Test send simple mail con default CC avvenuto con successo!</p>");
-//		assertTrue(check);
-//	}
-//
-//	// test default cc and ccn
-//	@Test
-//	public void sendSimpleMailWithDefaultCcAndCcn() {
-//
-//		logger.info("#########");
-//		logger.info("TEST - sendSimpleMailWithDefaultCcAndCcn()");
-//		logger.info("#########");
-//
-//		Boolean check = false;
-//		check = MailUtility.sendSimpleMailWithDefaultCcAndCcn(to, "Prova e-email con default CC e CCN",
-//				"<p class=\"abcde\">Test send simple mail con default CC e CCN avvenuto con successo!</p>");
-//		assertTrue(check);
-//	}
+
+	// test bcc
+	@Test
+	public void sendSimpleMailWithCcAndCcn() {
+
+		logger.info("#########");
+		logger.info("TEST - sendSimpleMailWithCcAndCcn()");
+		logger.info("#########");
+		Boolean check = false;
+		check = MailUtility.sendSimpleMailWithCcAndCcn(to, cc, ccn, "Prova e-mail con CC e CCN",
+				"<p class=\"abcde\">Test send simple mail con CC e CCN avvenuto con successo!</p>");
+		assertTrue(check);
+	}
+
+	// test default cc
+	@Test
+	public void sendSimpleMailWithDefaultCc() {
+
+		logger.info("#########");
+		logger.info("TEST - sendSimpleMailWithDefaultCc()");
+		logger.info("#########");
+
+		Boolean check = false;
+		check = MailUtility.sendSimpleMailWithDefaultCc(to, "Prova e-mail con default CC",
+				"<p class=\"abcde\">Test send simple mail con default CC avvenuto con successo!</p>");
+		assertTrue(check);
+	}
+
+	// test default cc and ccn
+	@Test
+	public void sendSimpleMailWithDefaultCcAndCcn() {
+
+		logger.info("#########");
+		logger.info("TEST - sendSimpleMailWithDefaultCcAndCcn()");
+		logger.info("#########");
+
+		Boolean check = false;
+		check = MailUtility.sendSimpleMailWithDefaultCcAndCcn(to, "Prova e-email con default CC e CCN",
+				"<p class=\"abcde\">Test send simple mail con default CC e CCN avvenuto con successo!</p>");
+		assertTrue(check);
+	}
 
 }
